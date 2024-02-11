0ea96668870032bc7f97b76ab49acee518d34ccc
mintuguptha <krishnavamshit827@gmail.com>
2024-02-11 22:10:43 +0530

    adding body component/images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ContentBodyComponent } from './content-body/content-body.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ContentBodyComponent
   ],
   imports: [
     BrowserModule,
